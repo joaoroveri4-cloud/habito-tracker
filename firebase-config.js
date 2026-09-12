9aa792ee21815f10e28d8e9d4dd395b40744a0a9
joaoroveri4-cloud <joao.roveri4@gmail.com>
2026-09-12 16:35:20 -0300

    feat: adiciona firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// ==========================================
+// CONFIGURAÇÃO DO FIREBASE FIRESTORE
+// Projeto: Atividade Final (atividade-final-8fb49)
+// ==========================================
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCbSYjZT1pT5BWm403O1J35Db641GCByzg",
+  authDomain: "atividade-final-8fb49.firebaseapp.com",
+  projectId: "atividade-final-8fb49",
+  storageBucket: "atividade-final-8fb49.firebasestorage.app",
+  messagingSenderId: "855951691407",
+  appId: "1:855951691407:web:120165f42928e63094eafa",
+  measurementId: "G-6BYCCRM8GB"
+};
